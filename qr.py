d31eed5ff9a6c8e454e7b205299c66ae87058dcb
FRLazcano <a01366080@itesm.mx>
2020-02-10 14:44:25 -0600

    hash
    se paso el hash a su propia funcion

--- qr.py
+++ qr.py
@@ -2,13 +2,16 @@ import pyqrcode
 import hashlib
 
 def createQR(direccion,datos,formato):
-    h=hashlib.md5()
-    h.update(datos.encode('utf-8'))
-    img=pyqrcode.create(h.hexdigest())
+    img=pyqrcode.create(datos)
     if formato:
         img.png(direccion)
     else:
         img.svg(direccion)
     return
 
-createQR('img.png','esta es una prueba',True)
+def hash2(str):
+    h=hashlib.md5()
+    h.update(str.encode('utf-8'))
+    return h.hexdigest()
+
+createQR('img.png',hash2('Esta es una prueba'),True)
