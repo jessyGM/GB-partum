cc27b6d80d14748cc0a423ed9b9b93f5462e8ba2
FRLazcano <a01366080@itesm.mx>
2020-02-13 14:02:26 -0600

    optimizacion
    se elimino código redundante

--- window.py
+++ window.py
@@ -1,20 +1,19 @@
 import qr_reader as reader
-import cv2
+import cv2 as cv
 import data_display as display
 
 def show_webcam(mirror=False):
-    cam = cv2.VideoCapture(0)
+    cam = cv.VideoCapture(0)
     while True:
         ret_val, img = cam.read()
         if mirror:
-            img = cv2.flip(img, 1)
-        cv2.imshow('my webcam', img)
-        decoded=reader.decode(img)
-        for obj in decoded:
+            img = cv.flip(img, 1)
+        cv.imshow('my webcam', img)
+        for obj in reader.decode(img):
           display.openWindow(obj)
-        if cv2.waitKey(1) == 27:
+        if cv.waitKey(1) == 27:
             break  # esc to quit
-    cv2.destroyAllWindows()
+    cv.destroyAllWindows()
 
 
 def main():
